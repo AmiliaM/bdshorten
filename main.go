65eeb66e6c85472105ab82155cbfa37267eb4f2b
AmiliaM <amiliamac@gmail.com>
2019-04-28 21:43:03 -0400

    Basic request methods

--- main.go
+++ main.go
@@ -7,16 +7,38 @@ import (
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s", r.URL.Path)
+	if r.URL.Path == "/" {
+		switch r.Method {
+		case "GET":
+			fmt.Fprintf(w, "All links")
+		case "POST":
+			fmt.Fprintf(w, "Created a new short URL")
+		case "DELETE":
+			fmt.Fprintf(w, "Deleted all links")
+		}
+		return
+	}
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, "The destination for %s", r.URL.Path[1:])
+	case "DELETE":
+		fmt.Fprintf(w, "Deleted short URL %s", r.URL.Path[1:])
+	}
 }
 
 func inviteHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is an invite")
 }
 
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Create a new short URL</h1>")
+}
+
 func main() {
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/invite/", inviteHandler)
+	http.HandleFunc("/new/", createHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
